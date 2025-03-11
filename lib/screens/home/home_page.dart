3518373e36f7468c72b08884b6431bbe825518ba
sikwan <yysskk99999@naver.com>
2025-03-11 14:37:49 +0900

    home design add

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:timeline_tile/timeline_tile.dart';
 import 'package:dtxproject/controllers/auth_controller.dart';
-import 'package:dtxproject/utils/date_utils.dart'; // DateUtil 클래스 import
+import 'package:dtxproject/utils/date_utils.dart';
 
 class HomePage extends StatelessWidget {
   final String? goalTitle;
@@ -17,7 +18,6 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final AuthController authController = Get.find<AuthController>();
 
-    // DateUtil 클래스를 사용하여 날짜 정보 가져오기
     final dateInfo = DateUtil.getCurrentDateInfo();
     final day = dateInfo['day']!;
     final weekday = dateInfo['weekday']!;
@@ -27,10 +27,8 @@ class HomePage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            // 상단 고정 영역 (스크롤 불가)
             Column(
               children: [
-                // 사용자 이름 (오른쪽 상단)
                 Padding(
                   padding: const EdgeInsets.only(top: 10, right: 20),
                   child: Align(
@@ -57,8 +55,6 @@ class HomePage extends StatelessWidget {
                         )),
                   ),
                 ),
-
-                // 흰색 배경 영역
                 Container(
                   color: Colors.white,
                   padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -242,72 +238,67 @@ class HomePage extends StatelessWidget {
                 ),
               ],
             ),
-
             Expanded(
               child: Container(
                 color: Colors.grey[100],
                 child: Column(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 20,
-                        right: 20,
-                        top: 18,
-                        bottom: 16,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              // 매일 미션 왼쪽 원 위치 조정 (카드 왼쪽 원과 중심 일직선 맞춤)
-                              SizedBox(
-                                width: 12,
-                                child: Center(
-                                  child: Container(
-                                    width: 12,
-                                    height: 12,
-                                    decoration: const BoxDecoration(
-                                      color: Color(0xFF707070),
-                                      shape: BoxShape.circle,
-                                    ),
+                    Container(
+                      height: 80,
+                      color: Colors.grey[100],
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 20,
+                          right: 20,
+                          top: 10,
+                          bottom: 10,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  width: 12,
+                                  height: 12,
+                                  decoration: const BoxDecoration(
+                                    color: Color(0xFF707070),
+                                    shape: BoxShape.circle,
+                                  ),
+                                ),
+                                const SizedBox(width: 12),
+                                const Text(
+                                  '매일 미션',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              width: 148,
+                              height: 47,
+                              child: ElevatedButton.icon(
+                                onPressed: () {},
+                                icon: const Icon(Icons.chat_bubble_outline,
+                                    size: 16),
+                                label: const Text('채팅상담'),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.blue,
+                                  foregroundColor: Colors.white,
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 12),
+                                  textStyle: const TextStyle(fontSize: 14),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
                                   ),
                                 ),
                               ),
-                              const SizedBox(width: 5),
-                              const Text(
-                                '매일 미션',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
-                          // 채팅상담 버튼 크기 조정 (148x47)
-                          SizedBox(
-                            width: 148,
-                            height: 47,
-                            child: ElevatedButton.icon(
-                              onPressed: () {
-                                // 채팅상담 기능 구현
-                              },
-                              icon: const Icon(Icons.chat_bubble_outline,
-                                  size: 16),
-                              label: const Text('채팅상담'),
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.blue,
-                                foregroundColor: Colors.white,
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 12),
-                                textStyle: const TextStyle(fontSize: 14),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                     Expanded(
@@ -317,13 +308,23 @@ class HomePage extends StatelessWidget {
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              _buildMissionItem('아침 기록', true),
-                              _buildMissionItem('점심 기록', true),
-                              _buildMissionItem('저녁 기록', true),
-                              _buildMissionItem('간식/야식 기록', true),
-                              _buildMissionItem('운동 기록', false),
-                              _buildMissionItem('오늘 하루 별점리뷰', false),
-                              _buildMissionItem('체중 기록', false),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 0),
+                                child: _buildTimelineMissionItem(
+                                    '아침 기록', true, true, false),
+                              ),
+                              _buildTimelineMissionItem(
+                                  '점심 기록', true, false, false),
+                              _buildTimelineMissionItem(
+                                  '저녁 기록', true, false, false),
+                              _buildTimelineMissionItem(
+                                  '간식/야식 기록', true, false, false),
+                              _buildTimelineMissionItem(
+                                  '운동 기록', false, false, false),
+                              _buildTimelineMissionItem(
+                                  '오늘 하루 별점리뷰', false, false, false),
+                              _buildTimelineMissionItem(
+                                  '체중 기록', false, false, true),
                               const SizedBox(height: 16),
                             ],
                           ),
@@ -340,133 +341,123 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Widget _buildMissionItem(String title, bool hasSwitch) {
-    // 토글 상태를 관리하기 위한 RxBool 변수
+  Widget _buildTimelineMissionItem(
+      String title, bool hasSwitch, bool isFirst, bool isLast) {
     final RxBool isToggled = false.obs;
 
-    return Stack(
-      children: [
-        // 원들의 중심을 연결하는 직사각형 (배경에 위치)
-        Positioned(
-          left: 4, // 원의 중심에 맞춤
-          top: 12, // 상단 원 아래에 패딩 추가
-          child: Container(
-            width: 4,
-            height: 73, // 카드 높이와 마진을 고려한 높이
-            color: const Color(0xFFD3D3D3), // 연한 회색
+    return TimelineTile(
+      isFirst: isFirst,
+      isLast: isLast,
+      alignment: TimelineAlign.manual,
+      lineXY: 0.02,
+      indicatorStyle: IndicatorStyle(
+        width: 8,
+        height: 8,
+        indicator: Container(
+          decoration: const BoxDecoration(
+            color: Color(0xFF707070),
+            shape: BoxShape.circle,
           ),
         ),
-        // 기존 Row 위젯
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // 카드 외부에 원 배치 (중심이 매일 미션 원과 일직선 맞춤)
-            SizedBox(
-              width: 12,
-              child: Center(
-                child: Container(
-                  width: 8,
-                  height: 8,
-                  decoration: const BoxDecoration(
-                    color: Color(0xFF707070),
-                    shape: BoxShape.circle,
+        padding: const EdgeInsets.all(2),
+        drawGap: true,
+      ),
+      beforeLineStyle: const LineStyle(
+        color: Color(0xFFD3D3D3),
+        thickness: 2,
+      ),
+      afterLineStyle: const LineStyle(
+        color: Color(0xFFD3D3D3),
+        thickness: 2,
+      ),
+      startChild: const SizedBox(
+        width: 100,
+      ),
+      endChild: Container(
+        constraints: const BoxConstraints(
+          minHeight: 85,
+        ),
+        padding: const EdgeInsets.only(left: 12, top: 4, bottom: 4),
+        child: Container(
+          width: double.infinity,
+          height: 77,
+          decoration: BoxDecoration(
+            color: const Color(0xFFE0E0E0),
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.05),
+                blurRadius: 2,
+                offset: const Offset(0, 1),
+              ),
+            ],
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
                   ),
                 ),
-              ),
-            ),
-            const SizedBox(width: 5),
-            // 카드 컨테이너
-            Expanded(
-              child: Container(
-                width: 315,
-                height: 77, // 높이 77로 설정
-                margin: const EdgeInsets.symmetric(vertical: 4),
-                decoration: BoxDecoration(
-                  color: const Color(0xFFE0E0E0), // 회색 배경으로 변경
-                  borderRadius: BorderRadius.circular(12),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.05),
-                      blurRadius: 2,
-                      offset: const Offset(0, 1),
-                    ),
-                  ],
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        title,
-                        style: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      if (hasSwitch)
-                        Obx(() => GestureDetector(
-                              onTap: () {
-                                isToggled.value = !isToggled.value;
-                              },
-                              child: Container(
-                                width: 70,
-                                height: 30,
-                                decoration: BoxDecoration(
-                                  color:
-                                      const Color(0xFF707070), // 더 어두운 회색으로 변경
-                                  borderRadius: BorderRadius.circular(15),
-                                ),
-                                child: Stack(
-                                  children: [
-                                    // 단식 텍스트 위치 조정
-                                    AnimatedPositioned(
-                                      duration:
-                                          const Duration(milliseconds: 200),
-                                      left: isToggled.value ? 10 : null,
-                                      right: isToggled.value ? null : 10,
-                                      top: 0,
-                                      bottom: 0,
-                                      child: const Center(
-                                        child: Text(
-                                          '단식',
-                                          style: TextStyle(
-                                            fontSize: 12,
-                                            color:
-                                                Colors.white, // 텍스트 색상 흰색으로 변경
-                                            fontWeight: FontWeight.w500,
-                                          ),
-                                        ),
-                                      ),
+                if (hasSwitch)
+                  Obx(() => GestureDetector(
+                        onTap: () {
+                          isToggled.value = !isToggled.value;
+                        },
+                        child: Container(
+                          width: 70,
+                          height: 30,
+                          decoration: BoxDecoration(
+                            color: const Color(0xFF707070),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Stack(
+                            children: [
+                              AnimatedPositioned(
+                                duration: const Duration(milliseconds: 200),
+                                left: isToggled.value ? 10 : null,
+                                right: isToggled.value ? null : 10,
+                                top: 0,
+                                bottom: 0,
+                                child: const Center(
+                                  child: Text(
+                                    '단식',
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w500,
                                     ),
-                                    // 원형 버튼 위치 조정
-                                    AnimatedAlign(
-                                      duration:
-                                          const Duration(milliseconds: 200),
-                                      alignment: isToggled.value
-                                          ? Alignment.centerRight
-                                          : Alignment.centerLeft,
-                                      child: Container(
-                                        width: 30,
-                                        height: 30,
-                                        decoration: const BoxDecoration(
-                                          color: Colors.white,
-                                          shape: BoxShape.circle,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
+                                  ),
                                 ),
                               ),
-                            )),
-                    ],
-                  ),
-                ),
-              ),
+                              AnimatedAlign(
+                                duration: const Duration(milliseconds: 200),
+                                alignment: isToggled.value
+                                    ? Alignment.centerRight
+                                    : Alignment.centerLeft,
+                                child: Container(
+                                  width: 30,
+                                  height: 30,
+                                  decoration: const BoxDecoration(
+                                    color: Colors.white,
+                                    shape: BoxShape.circle,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )),
+              ],
             ),
-          ],
+          ),
         ),
-      ],
+      ),
     );
   }
 }
